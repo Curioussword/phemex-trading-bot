d2c2523d4c08467daab1b84cab88745f6ee98521
bot reed <vmbot@vmi2251311.contaboserver.net>
2024-12-08 03:38:14 +0100

    I have updated a new implementation on how trades are taken. First off, we implemented a neutral zone that prevents false signals. Also we have dynamically adjusted to better align signals for buy and sell. On top of that, we can now see dynamically our unrealized profit and loss. Other implementations have been adjusted as well

--- src/indicators.py
+++ src/indicators.py
@@ -1,12 +1,44 @@
 import pandas as pd
 
+
+
 def calculate_vwap(data):
-    """Calculate VWAP (Volume Weighted Average Price)"""
-    q = data['volume']
-    p = data['close']
-    vwap = (p * q).cumsum() / q.cumsum()
+
+    if "close" not in data.columns or "volume" not in data.columns:
+
+        raise ValueError("Data missing required columns for VWAP calculation.")
+
+    #Ensures no zero or nan values
+    data["price_volume"] = data["close"] * data["volume"]
+
+    vwap = data["price_volume"].cumsum() / data["volume"].cumsum()
+
     return vwap
+
+
 
 def calculate_ema(data, period):
     """Calculate Exponential Moving Average (EMA)"""
     return data['close'].ewm(span=period, adjust=False).mean()
+
+
+
+# Calculate ATR over a rolling period
+def calculate_atr(data, period=14):
+    """
+    Calculate the Average True Range (ATR) indicator based on historical data.
+
+    """
+    # Calculate True Range
+    data['HL'] = data['high'] - data['low']
+    data['HC'] = abs(data['high'] - data['close'].shift())
+    data['LC'] = abs(data['low'] - data['close'].shift())
+    data['TR'] = data[['HL', 'HC', 'LC']].max(axis=1)
+
+    # Calculate the rolling average of TR over the 'period' days
+
+    data['ATR'] = data['TR'].rolling(window=period).mean()
+
+    # Return the most recent ATR value
+
+    return data['ATR'].iloc[-1]
